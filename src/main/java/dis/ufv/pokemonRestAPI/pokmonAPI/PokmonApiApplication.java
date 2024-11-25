acb6891402155ab4e08fa1ff1b7c1b8842c079c0
Dani <dani@gmail.com>
2024-11-25 19:57:07 +0100

    Commit inicial

---
+++ src/main/java/dis/ufv/pokemonRestAPI/pokmonAPI/PokmonApiApplication.java
@@ -0,0 +1,13 @@
+package dis.ufv.pokemonRestAPI.pokmonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokmonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokmonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dis/ufv/pokemonRestAPI/pokmonAPI/PokmonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dis.ufv.pokemonRestAPI.pokmonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokmonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
